803cfc0d569aa1d94b64145e59a09e7712d1b435
kumchovylcho <tamerchoyy@gmail.com>
2023-05-24 01:12:05 +0300

    now can change password and username

--- ribarite_na_noi/User/forms.py
+++ ribarite_na_noi/User/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import (UserCreationForm,
+                                       AuthenticationForm,
+                                       PasswordChangeForm,
+                                       UserChangeForm,
+                                       )
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 
@@ -88,3 +92,96 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
+
+
+class ChangePasswordForm(PasswordChangeForm):
+
+    old_password = forms.CharField(
+        label="",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"autofocus": True,
+                   "class": "form-control",
+                   "placeholder": "Current password",
+                   }
+        ),
+    )
+
+    new_password1 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "New password",
+                   "class": "form-control",
+                   }
+        ),
+        strip=False,
+
+    )
+    new_password2 = forms.CharField(
+        label="",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "New password confirmation",
+                   "class": "form-control",
+                   }
+        ),
+    )
+
+
+class ChangeUsernameForm(forms.ModelForm):
+    USERNAME_MAX_LENGTH = 20
+    USERNAME_MIN_LENGTH = 3
+
+    old_username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'old-username',
+                'class': 'form-control',
+                'autofocus': True,
+            }
+        ),
+        label="",
+    )
+
+    new_username = forms.CharField(
+        max_length=USERNAME_MAX_LENGTH,
+        required=True,
+        validators=(
+            validate_name,
+            MinLengthValidator(USERNAME_MIN_LENGTH),
+        ),
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'new-username',
+                'class': 'form-control',
+            }
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = User
+        fields = ("old_username", "new_username")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        old_username = cleaned_data.get('old_username')
+        new_username = cleaned_data.get('new_username')
+
+        old_user = User.objects.filter(username=old_username)
+        new_user = User.objects.filter(username=new_username)
+
+        if not old_user:
+            raise forms.ValidationError("Invalid old username")
+
+        if old_username == new_username:
+            raise forms.ValidationError("New username cannot be same as old username")
+
+        if new_user.exists():
+            raise forms.ValidationError("Username already exists")
+
+    def save(self, commit=True):
+        user = User.objects.filter(username=self.cleaned_data.get("old_username")).get()
+        user.username = self.cleaned_data.get("new_username")
+        user.save()
