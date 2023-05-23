5a6b4b1e8ef8a6e8e1afe95092c4f87b09d597a9
kumchovylcho <tamerchoyy@gmail.com>
2023-05-24 01:14:15 +0300

    added 404 class handler

--- ribarite_na_noi/common/views.py
+++ ribarite_na_noi/common/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -8,3 +9,10 @@ class HomeView(TemplateView):
 
 class WelcomePageView(TemplateView):
     template_name = 'welcome-page.html'
+
+
+class PageNotFound(TemplateView):
+    template_name = '404.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        return HttpResponseNotFound(render(request, self.get_template_names()), status=404)
