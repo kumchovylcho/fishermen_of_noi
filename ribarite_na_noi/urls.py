d7a7d48dace1bbe1f73b757790aee4c423c942dc
kumchovylcho <tamerchoyy@gmail.com>
2023-05-24 01:11:14 +0300

    added 404 error handler

--- ribarite_na_noi/urls.py
+++ ribarite_na_noi/urls.py
@@ -1,5 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (path,
+                         include,
+                         )
+from ribarite_na_noi.common.views import PageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +15,11 @@ urlpatterns = [
     path('braids/', include('ribarite_na_noi.Braids.urls')),
     path('chepareta/', include('ribarite_na_noi.Chepareta.urls')),
     path('leads/', include('ribarite_na_noi.Leads.urls')),
-
+    path('404/', PageNotFound.as_view(), name='404'),
 ]
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT
+                      )
+
+handler404 = PageNotFound.as_view()
