9a8002a8a63438406ee4d606f8c1dbd94b35ae6f
kumchovylcho <tamerchoyy@gmail.com>
2023-06-01 14:37:52 +0300

    added one more validation for username and added better TEST methods

--- ribarite_na_noi/common/validators.py
+++ ribarite_na_noi/common/validators.py
@@ -1,44 +1,72 @@
 import re
 
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, PermissionRequiredMixin
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
 
 
 def validate_name(value):
-    if not bool(re.finditer(r'^\w+$', value)):
+    if value.count("_") > 1:
+        raise ValidationError(
+            "You can't use only underscores or have more than one underscore"
+        )
+
+    if not list(re.finditer(r'^\w+$', value)):
         raise ValidationError(
             "You can only use letters, numbers and underscores"
         )
 
 
-class RedirectNotLoggedUsers(UserPassesTestMixin):
+class RedirectNotAuthorizedUsers(PermissionRequiredMixin, UserPassesTestMixin):
     user_must_be_logged = True
     no_permission_redirect_to = 'home'
 
     def test_func(self):
-        if self.user_must_be_logged:
-            return self.request.user.is_authenticated
-        return not self.request.user.is_authenticated
+        user = self.request.user
+
+        if self.user_must_be_logged and user.is_authenticated:
+            if any(x == True for x in (user.is_superuser,
+                                       self.check_obj_owner(user),
+                                       user.has_perm(self.permission_required),
+                                       )):
+                return True
+
+        return False
+
+    def check_obj_owner(self, user):
+        item = self.get_object()
+
+        if hasattr(item, 'created_by'):
+            return item.created_by.pk == user.pk
+
+        return item.pk == user.pk
+
+    def has_permission(self):
+        user = self.request.user
+        group_name = "staff"
+
+        if user.pk == self.get_object().created_by.pk:
+            return True
+
+        if user.is_superuser:
+            return True
+
+        if super().has_permission():
+            if user.groups.filter(name=group_name).exists():
+                return True
+
+        return False
 
     def handle_no_permission(self):
         return redirect(self.no_permission_redirect_to)
 
 
-class RedirectLoggedUsersUrlTypers(RedirectNotLoggedUsers):
+class RedirectNotAuthenticatedUsers(UserPassesTestMixin):
+    redirect_to = "home"
+    user_must_be_authenticated = True
 
     def test_func(self):
-        is_authenticated = super().test_func()
+        return self.request.user.is_authenticated
 
-        item = self.get_object()
-
-        if self.request.user.is_superuser:
-            return True
-
-        if self.request.user.groups.filter(name="staff").exists():
-            return True
-
-        if hasattr(item, 'created_by'):
-            return item.created_by.pk == self.request.user.pk and is_authenticated
-
-        return item.pk == self.request.user.pk and is_authenticated
+    def handle_no_permission(self):
+        return redirect(self.redirect_to)
