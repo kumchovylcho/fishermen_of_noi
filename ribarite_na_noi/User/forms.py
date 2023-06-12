da0530972b536b2b6119f8ea73c1b707f747a4fa
kumchovylcho <tamerchoyy@gmail.com>
2023-06-12 18:39:35 +0300

    update

--- ribarite_na_noi/User/forms.py
+++ ribarite_na_noi/User/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import (UserCreationForm,
                                        AuthenticationForm,
                                        PasswordChangeForm,
-                                       UserChangeForm,
                                        )
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
